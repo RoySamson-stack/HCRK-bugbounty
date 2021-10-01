a3f7ac7b79f84f690f38fe257f9a17f03eaee371
RoySamson-stack <rsam9009@gmail.com>
2021-10-01 11:59:23 +0300

    adding a signup page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import NavBar from './components/navbar'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from './components/home'
+import Signup from './components/signup'
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Switch>
           <Route path="/" component={Home} exact />
+        </Switch>
+        <Switch>
+          <Route path="/signup" component={Signup} exact />
         </Switch>
      </BrowserRouter>
   );
---
+++ src/components/signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Signup(props) {
+  return (
+    <div className="">
+      <form className="">
+        <input className="" placeholder="Enter your name or alias"/>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
