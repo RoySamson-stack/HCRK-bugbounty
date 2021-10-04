46533ff5e05301b05e18f9bf55387ea1b97ae2e2
RoySamson-stack <rsam9009@gmail.com>
2021-10-04 16:43:19 +0300

    creating a grid systems for the home page for showcase

--- src/components/home.js
+++ src/components/home.js
@@ -13,6 +13,10 @@ function Home() {
          HCRK intorduces a world where cyber security is connected in the country to connect comapnies and hackers
          so as to make a safe cyber space for use.
       </p>
+      <div className="grid grid-cols-2 gap-2  ">
+        <div className="bg-gray-300 h-20 w-200">hello</div>
+        <div className="bg-gray-300 h-20 w-200">hello</div>
+      </div>
       <div className="">
         <button type="" className="bg-blue-500 rounded ml-4 p-2">Try HCKR</button>
       </div>
--- src/components/signup.js
+++ src/components/signup.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function Signup(props) {
   return (
-    <div className="">
-      <form className="">
-        <input className="" placeholder="Enter your name or alias"/>
+    <div className="m-7 bg-gray-300 rounded h-full">
+      <form className="border-gray-200 w-1/5 form hover:border-light-blue-500">
+        <input className="m-4" placeholder="Enter your name or alias"/>
+        <input className="m-4" placeholder="Enter your email"/>
+        <input className="m-4" placeholder="Enter your name or alias"/>
+        <input
+          type="submit"
+          className="m-4 w-32 bg-blue-500 rounded hover:bg-blue-300"/>
       </form>
     </div>
   );
